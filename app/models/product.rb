79c1473b49546cc7764d82caca20a46df89cd0ed
sehrguey ogoltsoff <ogoltsoff@gmail.com>
2012-10-20 17:52:00 -0400

    Adding a Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-
+  has_many :line_items
+  
+  
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
   
   validates :title, :description, :image_url, presence: true
   
@@ -16,5 +20,27 @@ class Product < ActiveRecord::Base
   
   }
   validates :title, length: {minimum: 10}
+  
+  private
+  
+  
+  
+  # ensure that there are no line items referencing this product
+  
+  def ensure_not_referenced_by_any_line_item
+    
+    if line_items.empty?
+      
+      return true
+      
+    else
+      
+      errors.add(:base, 'Line Items present')
+                 
+                 return false
+                
+                end
+                
+                end
   
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -75,6 +75,19 @@ class ProductsControllerTest < ActionController::TestCase
                                             
                                             end
 
+					    test "can't delete product in cart" do
+					    
+					    assert_difference('Product.count', 0) do
+					      
+					      delete :destroy, id: products(:ruby).to_param
+					      
+					    end
+					    
+					    
+					    
+					    assert_redirected_to products_path
+					    
+end
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete :destroy, id: @product
