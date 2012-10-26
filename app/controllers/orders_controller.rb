54a24158e16a2ee15b419ee82151d7f71a698bec
sehrguey ogoltsoff <ogoltsoff@gmail.com>
2012-10-26 15:55:37 -0400

    formats

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -64,6 +64,7 @@ class OrdersController < ApplicationController
                              Cart.destroy(session[:cart_id])
                              
                              session[:cart_id] = nil
+			     OrderNotifier.received(@order).deliver
                              
                              format.html { redirect_to store_url, notice: 
                                            
