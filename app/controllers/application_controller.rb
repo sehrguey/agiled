b106be493a786e9dc5d95672892f608cfa48a836
sehrguey ogoltsoff <ogoltsoff@gmail.com>
2012-10-27 13:02:22 -0400

    bol a herik a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authorize
   private
   
   
@@ -17,4 +18,31 @@ class ApplicationController < ActionController::Base
                      cart
                      
                      end
+		     
+		     protected
+		     
+		     
+		     
+		     def authorize
+		       if request.format == Mime::HTML 
+			 
+			 unless User.find_by_id(session[:user_id])
+			                                
+			                                redirect_to login_url, notice: "Please log in"
+			                               
+			                               end
+			                               
+			                               else
+			                               
+			                               authenticate_or_request_with_http_basic do |username, password|
+			                               
+			                               user = User.find_by_name(username)
+			                               
+			                               user && user.authenticate(password)
+			                               
+			                               end
+			                               
+			                               end
+		     
+		     end
 end
