c2053258e8b8477c8b023c7b473bed12db94f302
sehrguey ogoltsoff <ogoltsoff@gmail.com>
2012-10-30 16:13:51 -0400

    bundle gems

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: 'Sam Ruby <depot@example.com>'
+  default from: 'sehrguey <ogoltsoff@yandex.ru>'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
